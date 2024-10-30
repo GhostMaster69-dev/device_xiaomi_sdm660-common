LineageOS/android_device_nokia_sdm660-common

sdm660-common: Add a device specific FCM
Change-Id: I9a36e9b33bf8c73cc0a98d441b141c3c698bc955

BoardConfigCommon.mk
@@ -62,6 +62,7 @@ TARGET_USES_ION := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(PLATFORM_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/lineage/config/device_framework_matrix.xml

GhostMaster69-dev/device_xiaomi_sdm660-common

Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

BoardConfigCommon.mk
@@ -95,6 +95,7 @@ LOC_HIDL_VERSION := 3.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
     vendor/derp/config/device_framework_matrix.xml