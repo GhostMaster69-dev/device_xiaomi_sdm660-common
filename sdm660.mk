97fb3975e3352506f845578677008d88e2563206
GhostMaster69-dev <rathore6375@gmail.com>
2024-11-04 14:42:37 +0530

    sdm660-common: Drop RemovePackages for now
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -414,10 +414,6 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # RIL
 PRODUCT_PACKAGES += \
     libavservices_minijail.vendor \
